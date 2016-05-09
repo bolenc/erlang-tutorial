9efe2ff1720c934ef2caffc4b132112f35df131e
Chase Bolen <chase.bolen@gmail.com>
2016-05-09 01:01:34 -0400

    Working on concurrent application

--- events/src/evserv.erl
+++ events/src/evserv.erl
@@ -20,18 +20,48 @@ loop(S= #state{}) ->
       Pid ! {MsgRef, ok},
       loop(S#state{clients=NewClients});
     {Pid, MsgRef, {add, Name, Description, TimeOut}} ->
-      ...
-    {Pid, MsgRef, {cancel, Name}} ->
-      ...
-    {done, Name} ->
-      ...
-    shutdown ->
-      ...
-    {'DOWN', Ref, process, _Pid, _Reason} ->
-      ...
-    code_change ->
-      ...
+      case valid_datetime(TimeOut) of
+        true ->
+          EventPid = event:start_link(Name, TimeOut),
+          NewEvents = ordict:store(Name,
+                                    #event{name=Name,
+                                            description=Description,
+                                            pid=EventPid,
+                                            timeout = TimeOut},
+                                    S#state.events),
+          Pid ! {MsgRef, ok},
+          loop(S#state{events = NewEvents});
+        false ->
+          Pid ! {MsgRef, {error, bad_timeout}},
+          loop(S)
+      end;
+    {Pid, MsgRef, {cancel, Name}} -> true;
+      %%...
+    {done, Name} -> true;
+      %%...
+    shutdown -> true;
+      %%...
+    {'DOWN', Ref, process, _Pid, _Reason} -> true;
+      %%...
+    code_change -> true;
+      %%...
     Unknown ->
       io:format("Unknown message: ~p~n", [Unknown]),
-      loop(State)
+      loop(S)
   end.
+
+valid_datetime({Date, Time}) ->
+  try
+    calendar:valid_date(Date) andalso valid_time(Time)
+  catch
+    error:function_clause ->
+      false
+  end;
+valid_datetime(_) ->
+  false.
+
+valid_time({H,M,S}) -> valid_time(H, M, S).
+valid_time(H, M, S) when H >=0, H < 24,
+                        M >= 0, M < 60,
+                        S >= 0, S < 60 -> true;
+valid_time(_,_,_) -> false.
